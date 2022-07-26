463ceb96f735d9449bfc5a3ee633e28975b4d93b
netmera ios dev <sdk@netmera.com>
2022-07-26 19:07:24 +0300

    Update frameworks

---
+++ Frameworks/NetmeraNotificationContentExtension/NetmeraNotificationContentExtension.xcframework/ios-arm64_armv7/NetmeraNotificationContentExtension.framework/Headers/NotificationContentExtension.h
@@ -0,0 +1,19 @@
+//
+//  NotificationContentExtension.h
+//  NotificationContentExtension
+//
+//  Created by Netmera on 21.09.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NotificationContentExtension.
+FOUNDATION_EXPORT double NotificationContentExtensionVersionNumber;
+
+//! Project version string for NotificationContentExtension.
+FOUNDATION_EXPORT const unsigned char NotificationContentExtensionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NotificationContentExtension/PublicHeader.h>
+
+
+#import <NetmeraNotificationContentExtension/NetmeraNotificationContentExtension.h>
